90ce79638f2b7666090337577679bd88903d0dd7
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-15 23:37:39 +0400

    template default

--- src/marimushka/export.py
+++ src/marimushka/export.py
@@ -110,7 +110,7 @@ def _generate_index(
 
 def main(
     output: str | Path | None = "_site",
-    template: str | Path | None = "templates/default.html.j2",
+    template: str | Path | None = Path(__file__).parent / "templates" / "default.html.j2",
     notebooks: str | Path | None = "notebooks",
     apps: str | Path | None = "apps",
     logger_instance: Logger | None = None,
