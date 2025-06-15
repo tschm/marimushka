75089ce0a6e0b0b205b7d3d94483e2b4d42af811
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-15 13:27:28 +0400

    testing, testing, testing

--- src/tests/test_complete.py
+++ src/tests/test_complete.py
@@ -1,28 +1,23 @@
-import pytest
-
 from marimushka.export import main
 from loguru import logger
 
-@pytest.fixture
-def output(resource_dir):
-    return resource_dir / "output"
 
-def test_complete(resource_dir, output):
+def test_complete(resource_dir, tmp_path):
     main(notebooks=resource_dir / "notebooks",
          apps=resource_dir / "apps",
          template=resource_dir / "templates" / "default.html.j2",
-         output=resource_dir / "output",
+         output=tmp_path / "output",
          logger_instance=logger)
 
-    assert (output / "index.html").exists()
-    assert (output / "notebooks" ).exists()
-    assert (output / "apps" ).exists()
+    assert (tmp_path / "output" / "index.html").exists()
+    assert (tmp_path / "output" / "notebooks" ).exists()
+    assert (tmp_path / "output" / "apps" ).exists()
 
-    assert (output / "notebooks" / "penguins.html").exists()
-    assert (output / "notebooks" / "fibonacci.html").exists()
+    assert (tmp_path / "output" /  "notebooks" / "penguins.html").exists()
+    assert (tmp_path / "output" /  "notebooks" / "fibonacci.html").exists()
 
-    assert (output / "apps" / "charts.html").exists()
+    assert (tmp_path / "output" / "apps" / "charts.html").exists()
 
-    assert '<a href="notebooks/penguins.html" class="card-link">Open Notebook</a>' in (output / "index.html").read_text()
-    assert '<a href="notebooks/fibonacci.html" class="card-link">Open Notebook</a>' in (output / "index.html").read_text()
-    assert '<a href="apps/charts.html" class="card-link app">Open App</a>' in (output / "index.html").read_text()
+    assert '<a href="notebooks/penguins.html" class="card-link">Open Notebook</a>' in (tmp_path / "output" / "index.html").read_text()
+    assert '<a href="notebooks/fibonacci.html" class="card-link">Open Notebook</a>' in (tmp_path / "output" /  "index.html").read_text()
+    assert '<a href="apps/charts.html" class="card-link app">Open App</a>' in (tmp_path / "output" /  "index.html").read_text()
